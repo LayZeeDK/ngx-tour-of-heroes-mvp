76a9545fda6d89058f310867bcd137f047a012ee
Lars Gyrup Brink Nielsen <lgn@frontmatec.com>
2018-04-25 23:42:01 +0200

    chore: Lint.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -68,7 +68,7 @@ export class HeroService {
   /** POST: add a new hero to the server */
   addHero (hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      tap((h: Hero) => this.log(`added hero w/ id=${h.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
   }
