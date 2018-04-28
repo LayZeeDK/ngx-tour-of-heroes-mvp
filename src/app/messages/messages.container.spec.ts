16e8bf16e223794f83ac65cd932a0e987dec08b5
Lars Gyrup Brink Nielsen <lgn@frontmatec.com>
2018-04-29 00:35:49 +0200

    feat(messages): add container spec

---
+++ src/app/messages/messages.container.spec.ts
@@ -0,0 +1,29 @@
+import { MessageService } from '../message.service';
+import { MessagesContainerComponent } from './messages.container';
+
+describe('MessagesContainerComponent', () => {
+  let container: MessagesContainerComponent;
+  let service: MessageService;
+
+  beforeEach(() => {
+    service = new MessageService();
+    spyOn(service, 'clear');
+    container = new MessagesContainerComponent(service);
+  });
+
+  it('lists messages', () => {
+    service.add('And a one...');
+    service.add('And a two...');
+    service.add('And a three...');
+
+    expect(container.messages.length).toEqual(3);
+    container.messages.forEach(message =>
+      expect(message).toEqual(jasmine.any(String)));
+  });
+
+  it('clears messages', () => {
+    container.clearMessages();
+
+    expect(service.clear).toHaveBeenCalledTimes(1);
+  });
+});
