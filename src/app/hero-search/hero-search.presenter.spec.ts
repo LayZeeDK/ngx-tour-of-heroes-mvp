4fb32e6c59cbad06a01290454a94a0915fb8b529
Lars Gyrup Brink Nielsen <lgn@frontmatec.com>
2018-04-28 23:54:54 +0200

    test(hero-search): add presenter spec

---
+++ src/app/hero-search/hero-search.presenter.spec.ts
@@ -0,0 +1,93 @@
+import { fakeAsync, tick } from '@angular/core/testing';
+import { Subscription } from 'rxjs';
+
+import { HeroSearchPresenter } from './hero-search.presenter';
+
+describe('HeroSearchPresenter', () => {
+  beforeEach(() => {
+    presenter = new HeroSearchPresenter();
+  });
+
+  let presenter: HeroSearchPresenter;
+
+  describe('emits search terms', () => {
+    beforeEach(() => {
+      searchTermsSpy = jasmine.createSpy('searchTermsObserver');
+      searchTermsSubscription = presenter.searchTerms$
+        .subscribe(searchTermsSpy);
+    });
+
+    afterEach(() => {
+      searchTermsSubscription.unsubscribe();
+    });
+
+    const debounceTime = 300;
+    let searchTermsSpy: jasmine.Spy;
+    let searchTermsSubscription: Subscription;
+
+    it('when a user searches', fakeAsync(() => {
+      const gamora = 'gamora';
+
+      presenter.search(gamora);
+      tick(debounceTime);
+
+      expect(searchTermsSpy).toHaveBeenCalledTimes(1);
+      expect(searchTermsSpy).toHaveBeenCalledWith(gamora);
+    }));
+
+    it(`after ${debounceTime} milliseconds of inactivity`, fakeAsync(() => {
+      const elektra = 'elektra';
+
+      presenter.search(elektra);
+      tick(debounceTime - 1);
+
+      expect(searchTermsSpy).not.toHaveBeenCalled();
+      tick(1);
+
+      expect(searchTermsSpy).toHaveBeenCalledTimes(1);
+    }));
+
+    it(`that is the latest preceeded by at most ${debounceTime - 1} milliseconds of inactivity`, fakeAsync(() => {
+      const medusa = 'medusa';
+      const wasp = 'wasp';
+
+      presenter.search(medusa);
+      tick(debounceTime - 1);
+      presenter.search(wasp);
+
+      expect(searchTermsSpy).not.toHaveBeenCalled();
+      tick(debounceTime);
+
+      expect(searchTermsSpy).toHaveBeenCalledTimes(1);
+      expect(searchTermsSpy).toHaveBeenCalledWith(wasp);
+    }));
+
+    it('and ignores duplicates', fakeAsync(() => {
+      const sheHulk = 'she-hulk';
+
+      presenter.search(sheHulk);
+      tick(debounceTime - 1);
+      presenter.search(sheHulk);
+
+      expect(searchTermsSpy).not.toHaveBeenCalled();
+      tick(300);
+
+      expect(searchTermsSpy).toHaveBeenCalledTimes(1);
+      expect(searchTermsSpy).toHaveBeenCalledWith(sheHulk);
+    }));
+
+    it('but duplicates reset the inactivity period', fakeAsync(() => {
+      const scarletWitch = 'scarlet witch';
+
+      presenter.search(scarletWitch);
+      tick(debounceTime - 1);
+      presenter.search(scarletWitch);
+      tick(1);
+
+      expect(searchTermsSpy).not.toHaveBeenCalled();
+      tick(300);
+
+      expect(searchTermsSpy).toHaveBeenCalledTimes(1);
+    }));
+  });
+});
