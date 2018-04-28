5c64324e5eb10be5d2d68f9787c493a6f29fe5c8
Lars Gyrup Brink Nielsen <lgn@frontmatec.com>
2018-04-28 11:25:32 +0200

    chore: add Wallaby

---
+++ src/wallaby-test.ts
@@ -0,0 +1,15 @@
+import 'core-js/es7/reflect';
+import './polyfills';
+
+import 'zone.js/dist/zone-testing';
+
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
