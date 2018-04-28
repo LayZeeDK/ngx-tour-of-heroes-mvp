07e07e6e658b60319aba045eeea67e4b83d4d53d
Lars Gyrup Brink Nielsen <lgn@frontmatec.com>
2018-04-29 00:45:43 +0200

    feat(messages): add presenter spec

---
+++ src/app/messages/messages.presenter.spec.ts
@@ -0,0 +1,30 @@
+import { MessagesPresenter } from './messages.presenter';
+
+describe('MessagesPresenter', () => {
+  let presenter: MessagesPresenter;
+
+  beforeEach(() => {
+    presenter = new MessagesPresenter();
+  });
+
+  it('lists messages', () => {
+    expect(presenter.messages).toEqual(jasmine.any(Array));
+  });
+
+  it('indicates whether it has any messages', () => {
+    presenter.messages = [];
+
+    expect(presenter.hasMessages).toBe(
+      false,
+      'it must not indicate that it has any messages');
+    presenter.messages = [
+      'The city is flying, we’re fighting an army of robots and I have a bow and arrow. None of this makes sense.',
+      'There is nothing more reassuring than realizing the world is crazier than you are.',
+      'You know who I am. You don’t know where I am. And you’ll never see me coming.',
+    ];
+
+    expect(presenter.hasMessages).toBe(
+      true,
+      'it must indicate that it has messages');
+  });
+});
