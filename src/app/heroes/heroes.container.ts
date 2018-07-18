99bd25ad87f58b0ce5d7d50326f93fbcfc805080
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2018-07-19 01:26:26 +0200

    refactor(heroes): remove subscription and lifecycle hook
    We still need to merge the streams into a single stream of heroes.

--- src/app/heroes/heroes.container.ts
+++ src/app/heroes/heroes.container.ts
@@ -1,49 +1,59 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { skip } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+
+type HeroUpdate = 'add' | 'remove';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-heroes',
   templateUrl: './heroes.container.html',
 })
-export class HeroesContainerComponent implements OnInit {
-  private heroes: BehaviorSubject<Hero[]> = new BehaviorSubject([]);
-  heroes$: Observable<Hero[]> = this.heroes.asObservable().pipe(
-    skip(1),
+export class HeroesContainerComponent {
+  private addHero: Subject<Hero> = new Subject();
+  private heroes: Subject<Hero[]> = new Subject();
+  private initialHeroes = this.heroService.getHeroes().pipe(
+    shareReplay(1),
   );
+  private removeHero: Subject<Hero> = new Subject();
 
-  constructor(private heroService: HeroService) {}
+  heroes$: Observable<Hero[]>;
 
-  ngOnInit(): void {
-    this.heroService.getHeroes()
-      .subscribe(this.heroes);
+  constructor(private heroService: HeroService) {
+    this.heroes$ = this.initializeHeroes();
   }
 
   add(name: string): void {
     this.heroService.addHero({ name } as Hero)
-      .subscribe(h => this.addHero(h));
+      .subscribe(h => this.addHero.next(h));
   }
 
   delete(hero: Hero): void {
-    this.removeHero(hero);
+    this.removeHero.next(hero);
     this.heroService.deleteHero(hero)
       .subscribe(
         undefined,
-        () => this.addHero(hero));
+        () => this.addHero.next({ ...hero }));
   }
 
-  private addHero(hero: Hero): void {
-    this.heroes.next([
-      ...this.heroes.value,
-      hero,
-    ]);
+  private initializeHeroes(): Observable<Hero[]> {
+    // TOOD: create heroes observable from initialHeroes, addHero and removeHero
+    return new Subject<Hero[]>();
   }
 
-  private removeHero(hero: Hero): void {
-    this.heroes.next(this.heroes.value.filter(h => h !== hero));
+  private addToHeroes(hero: Hero, heroes: Hero[]): Hero[] {
+    return heroes.includes(hero)
+      ? heroes
+      : [
+        ...heroes,
+        hero,
+      ];
+  }
+
+  private removeFromHeroes(hero: Hero, heroes: Hero[]): Hero[] {
+    return heroes.filter(h => h !== hero);
   }
 }
